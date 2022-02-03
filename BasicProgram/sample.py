c9861a3d9fe7e399133eb6435b35e47b0c49ae7a
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-02-03 20:32:52 +0530

    [Farzana] Add: Python Program to print hello.

---
+++ BasicProgram/sample.py
@@ -0,0 +1,9 @@
+'''
+@Author: Farzana Shaikh
+@Date: 2022-02-03 20:00:30
+@Last Modified by: Farzana Shaikh
+@Last Modified time: 2021-02-03 20:00:30
+@Title : Program To Print hello
+'''
+
+print("hello")
